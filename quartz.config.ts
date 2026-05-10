6100663175313bbaa0795c1584535010aaed4a3f
goofyass1231561 <matej.divis08@gmail.com>
2026-05-10 18:24:55 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "cs-CZ",
+    baseUrl: "goofyass1231561.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
